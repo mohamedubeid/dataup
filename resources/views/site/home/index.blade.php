71c030554a50d2106d4fa126873b79b846441a45
mohamedubeid <mohammed.ubeid99@gmail.com>
2025-03-22 02:46:33 +0300

    working on home page

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -10,7 +10,7 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 @section('content')
 
-    <h1>{{ __('main.title') }}</h1>
+    {{-- <h1>{{ __('main.title') }}</h1>
     <p>{{ $homeBlocks->getTranslatedAttribute('title') }}</p>
     <ul>
         @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
@@ -21,6 +21,26 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
             </li>
         @endforeach
     </ul>
-    <a href="{{ LaravelLocalization::localizeUrl(route('about')) }}">{{ __('main.about_us') }}</a>
+    <a href="{{ LaravelLocalization::localizeUrl(route('about')) }}">{{ __('main.about_us') }}</a> --}}
+
+    <section>
+        <div class="container">
+            <div class="newsletter-sec">
+
+                <div class="d-flex flex-column gap-4">
+                    <h1 class="fw-bolder"><span class="gradient-text">Transforming</span> Data into Insights with Precision</h1>
+                    <p class="subtitle-style">We provide high-quality data annotation and curation services to help you build better AI models.</p>
+                    <form method="POST" action="javascript:void(0)" id="newsletterForm">
+                        @csrf
+                        <div class="d-flex flex-column flex-sm-row position-relative">
+                          <input name="email" type="email" placeholder="{{ __('main.email_placeholder')}}" aria-label="email" required class="{{ LaravelLocalization::getCurrentLocale() == 'en' ? 'text-end' : 'text-start' }}" />
+                          <button class="hero-btn">{{ __('main.submit')}}</button>
+                        </div>
+                        <div id="formResponse" class="w-100"></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
 
 @endsection
