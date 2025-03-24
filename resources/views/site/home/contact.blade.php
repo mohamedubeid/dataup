520b6190f63bfcf9457ba1e052983832257125b3
mohamedubeid <mohammed.ubeid99@gmail.com>
2025-03-24 20:55:45 +0200

    implemented contact us page

--- resources/views/site/home/contact.blade.php
+++ resources/views/site/home/contact.blade.php
@@ -6,9 +6,11 @@
 
 @section('content')
 
-<section>
-    <div class="container">
-        Contact
+<section class="margin-page">
+    <div class="container text-center">
+        <h2 class="mb-3 fw-bold">{{ __('main.we_love_to')}} <span class="primary-color"> {{ __('main.hear_from_you')}}</span> {{ __('main.from_you')}}</h2>
+        <p class="fs-5 mb-5 pb-2">{{ __('main.contact_message')}}</p>
+        @include('site.components.contact-form')
     </div>
 </section>
 
