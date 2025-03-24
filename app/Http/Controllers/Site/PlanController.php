783744f742322c69be230c423735be1698b6cdcf
mohamedubeid <mohammed.ubeid99@gmail.com>
2025-03-24 20:48:58 +0200

    implemented picing page

--- app/Http/Controllers/Site/PlanController.php
+++ app/Http/Controllers/Site/PlanController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use App\Models\Client;
 use App\Models\HomeBlock;
+use App\Models\Plan;
 
 class PlanController extends Controller
 {
     public function index() {
         $homeBlocks = HomeBlock::first();
-        return view('site.plans.index', compact('homeBlocks'));
+        $plans = Plan::orderBy('id', 'asc')
+				->take(3)->get();
+        return view('site.plans.index', compact('homeBlocks','plans'));
     }
 }
--- resources/views/site/plans/index.blade.php
+++ resources/views/site/plans/index.blade.php
@@ -8,9 +8,12 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 @section('desc', "Flexible Pricing Plans for Your Needs")
 
 @section('content')
-<section class="clients header-section">
+<section class="margin-page">
   <div class="container">
-    Plans
+
+      <h2 class="mb-5 pb-4 text-center"><span class="primary-color">{{ __('main.flexible')}}</span> {{ __('main.ppf')}}</h2>
+
+    @include('site.components.our-plans', ['plans' => $plans])
   </div>
 </section>
 @endsection
